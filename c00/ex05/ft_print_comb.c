00c495170ac3ac0f7fd127627863f876eed7faef
el Mehdi Boussoufi <117457862+Mehdi609@users.noreply.github.com>
2025-07-18 07:21:18 +0100

    ex05

---
+++ c00/ex05/ft_print_comb.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+
+void ft_putchar(char c){
+	write(1, &c, 1);
+}
+
+void ft_print_comb(void){
+	for(int a = 0 ; a <= 9; a++){
+
+		for(int b = 0 ; b <= 9; b++){
+			if(a == b||a>b){	
+			       	continue;
+			}
+				
+			for(int c = 0 ; c <= 9; c++){
+				
+				if(c == b||b>c){	
+				       	continue;
+				}
+				ft_putchar(a + '0');
+				ft_putchar(b + '0');
+				ft_putchar(c + '0');
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+		}
+	}
+}
+
+
+int main(){
+	ft_print_comb();
+	return 0;
+}
