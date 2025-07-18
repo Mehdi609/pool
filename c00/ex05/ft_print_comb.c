832701ae8ee0d208191333653ba4ad98e4b088f1
el Mehdi Boussoufi <117457862+Mehdi609@users.noreply.github.com>
2025-07-18 10:22:52 +0100

    clean code

--- c00/ex05/ft_print_comb.c
+++ c00/ex05/ft_print_comb.c
@@ -8,20 +8,24 @@ void ft_print_comb(void){
 	for(int a = 0 ; a <= 9; a++){
 
 		for(int b = 0 ; b <= 9; b++){
-			if(a == b||a>b){	
+			if(a>=b){	
 			       	continue;
 			}
 				
 			for(int c = 0 ; c <= 9; c++){
 				
-				if(c == b||b>c){	
+				if(b>=c){	
 				       	continue;
 				}
 				ft_putchar(a + '0');
 				ft_putchar(b + '0');
 				ft_putchar(c + '0');
-				ft_putchar(',');
-				ft_putchar(' ');
+				if(a != 7 || b != 8 || c != 9){
+			 		ft_putchar(',');
+					ft_putchar(' ');
+				}else {	
+			 		ft_putchar('\n');
+				}
 			}
 		}
 	}
